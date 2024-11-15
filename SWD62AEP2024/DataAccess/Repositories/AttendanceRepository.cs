f14a6c39b7b10b3479a8815aaef2604cfdd5e6df
Mqabbadbest <jean.briffa.i79575@mcast.edu.mt>
2024-11-15 09:28:05 +0100

    Started coding the attendance functionality. Began implementing and ListViewModel, a Repository and a Controller for this.

---
+++ SWD62AEP2024/DataAccess/Repositories/AttendanceRepository.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.DataContext;
+using Domain.Models;
+
+namespace DataAccess.Repositories
+{
+    public class AttendanceRepository
+    {
+        private AttendanceContext _attendanceContext;
+        public AttendanceRepository(AttendanceContext attendanceContext) {
+            _attendanceContext = attendanceContext;
+        }
+
+        public void AddAttendance(Attendance a)
+        {
+            a.Timestamp = DateTime.Now;
+            _attendanceContext.Attendances.Add(a);
+            _attendanceContext.SaveChanges();
+        }
+
+        public IQueryable<Attendance> GetAttendances(DateTime date, string groupCode, string subjectCode)
+        {
+            return _attendanceContext.Attendances.Where(a => a.Timestamp.Date == date.Date && a.SubjectFK == subjectCode && a.Student.GroupFK == groupCode);
+        }
+
+        public IQueryable<Attendance> GetAttendances()
+        {
+            return _attendanceContext.Attendances;
+        }
+    }
+}
---
+++ SWD62AEP2024/Presentation/Controllers/AttendanceController.cs
@@ -0,0 +1,51 @@
+﻿using DataAccess.DataContext;
+using DataAccess.Repositories;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+
+namespace Presentation.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        private AttendanceRepository _attendanceRepository;
+        public AttendanceController(AttendanceRepository attendanceRepository)
+        {
+            _attendanceRepository = attendanceRepository;
+        }
+
+        //A page which shows which attendances I can take
+        public IActionResult Index()
+        {
+            //Note: getting all attendances, but then extracting soe information which I will put on screen
+            //      being Subject, Date and Group
+            //...this requires me to create a ViewModel to accomodate this newly group of data 
+
+            var groupedAttendances = _attendanceRepository
+                .GetAttendances()
+                    .GroupBy(a => new { a.SubjectFK, a.Student.GroupFK, a.Timestamp.Date, Subject = a.Subject.Name })
+                    .OrderByDescending(x => x.Key.Date)
+                    .Select(g => new AttendancesListViewModel()
+                    {
+                        SubjectFK = g.Key.SubjectFK,
+                        Group = g.Key.GroupFK,
+                        SubjectName = g.Key.Subject,
+                        Date = g.Key.Date,
+                    })
+                    .ToList();
+            return View(groupedAttendances);
+        }
+
+        [HttpGet] //it needs to show me which students are supposed to be in that attendance list
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // it saves the absences and presences of all the studnets from the first Create method
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
