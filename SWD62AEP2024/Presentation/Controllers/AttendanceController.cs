74a10fa45e6a6f6f0a8a54730e900feab36db3d6
Mqabbadbest <jean.briffa.i79575@mcast.edu.mt>
2024-11-25 12:55:16 +0100

    Continued Implementing past attendances feature. Currently, the checkboxes are not working properly and the subject name is not showing the UI

--- SWD62AEP2024/Presentation/Controllers/AttendanceController.cs
+++ SWD62AEP2024/Presentation/Controllers/AttendanceController.cs
@@ -36,15 +36,15 @@ namespace Presentation.Controllers
                 {
                     Date = x.Timestamp,
                     SubjectCode = x.SubjectFK,
-                    SubjectName = x.Subject.Name,
-                    GroupCode = x.Student.GroupFK
+                    GroupCode = x.Student.GroupFK,
+                    SubjectName = x.Subject.Name
                 })
                 .Select(g => new SelectPastAttendanceViewModel
                 {
                     Date = g.Key.Date,
                     SubjectCode = g.Key.SubjectCode,
+                    GroupCode = g.Key.GroupCode,
                     SubjectName = g.Key.SubjectName,
-                    GroupCode = g.Key.GroupCode
                 })
                 .ToList();
 
@@ -59,32 +59,58 @@ namespace Presentation.Controllers
         }
 
         [HttpGet] //it needs to show me which students are supposed to be in that attendance list
-        public IActionResult Create(string groupCode, string subjectCode)
+        public IActionResult Create(string groupCode, string subjectCode, string attendanceButton)
         {
 
-            var students = _studentsRepository.GetStudents() //Select * from Students
-                .Where(s => s.GroupFK == groupCode)// Select * from Students where GroupFK = groupCode
-                .OrderBy(s => s.FirstName) // Order by FirstName
-                .ToList(); // Execute the query
+            if (attendanceButton == "0") {
+                var students = _studentsRepository.GetStudents() //Select * from Students
+                    .Where(s => s.GroupFK == groupCode)// Select * from Students where GroupFK = groupCode
+                    .OrderBy(s => s.FirstName) // Order by FirstName
+                    .ToList(); // Execute the query
 
-            CreateAttendanceViewModel viewModel = new CreateAttendanceViewModel()
-            {
-                GroupCode = groupCode,
-                SubjectCode = subjectCode,
-                Students = students
-            };
+                CreateAttendanceViewModel viewModel = new CreateAttendanceViewModel()
+                {
+                    GroupCode = groupCode,
+                    SubjectCode = subjectCode,
+                    Students = students
+                };
 
-            Subject subject = _subjectsRepository.GetSubjects().SingleOrDefault(s => s.Code == subjectCode);
-            if(subject != null)
-            {
-                viewModel.SubjectName = subject.Name;
+                Subject subject = _subjectsRepository.GetSubjects().SingleOrDefault(s => s.Code == subjectCode);
+                if (subject != null)
+                {
+                    viewModel.SubjectName = subject.Name;
+                }
+                else
+                {
+                    viewModel.SubjectName = "";
+                }
+
+                return View(viewModel);
             }
             else
             {
-                viewModel.SubjectName = "";
-            }
+                string[] myValues= attendanceButton.Split(new char[] { '|' });
+                DateTime date = Convert.ToDateTime(myValues[0]);
+                string selectedSubjectCode = myValues[1];
+                string selectedGroupCode = myValues[2];
 
-            return View(viewModel);
+                CreateAttendanceViewModel viewModel = new CreateAttendanceViewModel();
+                viewModel.GroupCode = selectedGroupCode;
+                viewModel.SubjectCode = selectedSubjectCode;
+                viewModel.Students = _studentsRepository.GetStudents().Where(s => s.GroupFK == selectedGroupCode).OrderBy(s => s.FirstName).ToList();
+                viewModel.Presence = _attendanceRepository.GetAttendances()
+                    .Where(
+                    s => s.SubjectFK == selectedSubjectCode 
+                    && s.Timestamp.Day == date.Day 
+                    && s.Timestamp.Month == date.Month 
+                    && s.Timestamp.Year == date.Year 
+                    && s.Timestamp.Minute == date.Minute)
+                    .OrderBy(s => s.Student.FirstName)
+                    .Select(s => s.IsPresent)
+                    .ToList();
+
+                return View(viewModel);
+            }
         }
 
         [HttpPost] // it saves the absences and presences of all the studnets from the first Create method
--- SWD62AEP2024/Presentation/Models/CreateAttendanceViewModel.cs
+++ SWD62AEP2024/Presentation/Models/CreateAttendanceViewModel.cs
@@ -5,9 +5,17 @@ namespace Presentation.Models
 {
     public class CreateAttendanceViewModel
     {
+        public CreateAttendanceViewModel()
+        {
+            Presence = new List<bool>();
+        }
+
+
         public List<Student> Students { get; set; }
         public string SubjectCode { get; set; }
         public string SubjectName { get; set; }
         public string GroupCode { get; set; }
+
+        public List<bool> Presence { get; set; }
     }
 }
